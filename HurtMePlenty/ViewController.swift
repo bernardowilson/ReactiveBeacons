74dad1b021484e1543b50cacf03b4e5899f530ea
Bernardo Alecrim <bernardo@alecrim.dev>
2019-07-14 15:25:15 -0400

    Initial Commit

---
+++ HurtMePlenty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HurtMePlenty
+//
+//  Created by Bernardo Alecrim on 7/14/19.
+//  Copyright © 2019 Bernardo Alecrim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
