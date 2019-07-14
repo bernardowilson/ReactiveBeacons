9e06931baf1e7fc4cdc3588dd419319e84676790
Bernardo Alecrim <bernardo@alecrim.dev>
2019-07-14 15:57:55 -0400

    Initial project with podfile.
    Signed-off-by: Bernardo Alecrim <bernardo@alecrim.dev>

---
+++ HurtMePlenty/Modules/AppCoordinator.swift
@@ -0,0 +1,11 @@
+//
+//  AppCoordinator.swift
+//  HurtMePlenty
+//
+//  Created by Bernardo Alecrim on 7/14/19.
+//  Copyright © 2019 Bernardo Alecrim. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ HurtMePlenty/Protocols/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  HurtMePlenty
+//
+//  Created by Bernardo Alecrim on 7/14/19.
+//  Copyright © 2019 Bernardo Alecrim. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    
+    
+    
+}
